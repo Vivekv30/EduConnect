41914cbe8f1d71c70c10c4c7da1305ce831a4e1a
Vivekv30 <viveknasena130@gmail.com>
2023-12-01 00:25:06 +0530

    added search bar can find students by name or emailid or mobile number or gender too

--- frontend/src/Components/Students/ListOfStudents.jsx
+++ frontend/src/Components/Students/ListOfStudents.jsx
@@ -6,27 +6,32 @@ import { Helmet } from 'react-helmet';
 const ListOfStudents = () => {
   const navigate = useNavigate();
   const [students, setStudents] = useState([]);
+  const [filterStudents, setFilterStudents] = useState([])
   const [isLoading, setIsLoading] = useState(false);
-
+  const [error, setError] = useState('')
   useEffect(() => {
     setIsLoading(true)
     StudentService.getStudents()
       .then(res => {
         setIsLoading(false)
         setStudents(res.data);
+        setFilterStudents(res.data)
       })
       .catch(error => {
+        setIsLoading(false);
+        setError('run backend application')
         console.log(error);
       });
   }, []);
 
   function deleteStudent(id) {
-    
+
     if (window.confirm("Do you want to delete this student?")) {
       StudentService.deleteStudent(id)
         .then(() => {
           window.alert("Student deleted successfully")
           setStudents(students.filter(student => student.id !== id));
+          setFilterStudents(filterStudents.filter(student => student.id !== id))
           navigate('/students'); // Navigate to the students page
         })
         .catch((error) => {
@@ -34,6 +39,26 @@ const ListOfStudents = () => {
         });
     }
   }
+
+  function handleSearch(e) {
+    const searchText = e.target.value.trim().toLowerCase();
+    if (!searchText) {
+      // If the search input is empty, display all students
+      setStudents(filterStudents);
+    } else {
+      // Filter students based on the search input
+      const filteredStudents = filterStudents.filter(student => {
+        return (
+          student.name.toLowerCase().includes(searchText) ||
+          student.emailId.toLowerCase().includes(searchText) ||
+          student.mobile.toLowerCase().includes(searchText) ||
+          student.gender.toLowerCase() === searchText
+        )
+      });
+      setStudents(filteredStudents);
+    }
+  }
+
 
   return (
     <div className='container ' style={{ display: 'flex' }}>
@@ -49,7 +74,19 @@ const ListOfStudents = () => {
                         <span className="sr-only">Loading...</span>
                     </div> */}
         </div>}
-        <h3>list of Students</h3>
+        {error && error !== '' && (
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        )}
+        <nav className="navbar ">
+          <div className="container-fluid">
+            <h3 className="navbar-brand">List of Students</h3>
+            <div className="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={handleSearch} />
+            </div>
+          </div>
+        </nav>
         <table className="table table-striped">
           <thead>
             <tr style={{ textAlign: 'center' }}>
